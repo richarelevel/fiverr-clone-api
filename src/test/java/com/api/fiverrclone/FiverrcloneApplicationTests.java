1c9960f8f493693f823dc9f2e57c82a11acdf2c5
richasinhaa <richa.sinha0603@gmail.com>
2022-01-12 18:31:11 +0800

    Fiverr Clone API template added

---
+++ src/test/java/com/api/fiverrclone/FiverrcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fiverrclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiverrcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
